732f667bdabea967c470b0143d2ea493045da0c6
BIT Student <classroom@bgit.rs>
2018-03-21 15:15:07 +0100

    Week 8, Day 3, improved search

--- ReactUsers_extra/reactusers/src/app/partials/Search.js
+++ ReactUsers_extra/reactusers/src/app/partials/Search.js
@@ -16,6 +16,12 @@ class Search extends React.Component {
         this.setState({ value: event.target.value });
     }
 
+    resetChange = () => {
+        this.props.changeValue("");
+
+        this.setState({ value: "" });
+    }
+
     render() {
         return (
             <div>
@@ -23,7 +29,7 @@ class Search extends React.Component {
                     <div className="input-field">
                         <input id="search" type="search" value={this.state.value} onChange={this.handleChange} placeholder="Search users" required />
                         <label className="label-icon" htmlFor="search"><i className="material-icons">search</i></label>
-                        <i className="material-icons">close</i>
+                        <i className="material-icons" onClick={this.resetChange}>close</i>
                     </div>
                 </form>
             </div>
