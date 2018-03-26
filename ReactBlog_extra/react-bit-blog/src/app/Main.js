b17291c32dfe2c3bef3ab187be74918163fbb666
JelenaMarjanovic <jelena.marjanovic.13@gmail.com>
2018-03-26 04:50:11 +0200

    [bbf-1] Add about page

--- ReactBlog_extra/react-bit-blog/src/app/Main.js
+++ ReactBlog_extra/react-bit-blog/src/app/Main.js
@@ -3,12 +3,14 @@ import { Switch, Route } from 'react-router-dom';
 
 import { PostsPage } from './posts/PostsPage';
 import { AuthorsPage } from './authors/AuthorsPage';
+import {AboutPage} from './about/AboutPage';
 
 const Main = () => (
     <main className="container">
         <Switch>
             <Route exact path="/" component={PostsPage} />
             <Route exact path="/authors" component={AuthorsPage} />
+            <Route exact path="/about" component={AboutPage} />
         </Switch>
     </main>
 );
---
+++ ReactBlog_extra/react-bit-blog/src/app/about/AboutPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const AboutPage = () => (
+    <div className="container blue-text text-darken-4">
+        <h4 className="center">ABOUT</h4>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate ipsam accusantium unde! Dolores voluptates earum sint? Mollitia tempora repellat officiis obcaecati! Vero corrupti eius nemo quisquam perspiciatis minima a sapiente!
+        In totam earum quos veritatis sed quo culpa nostrum quis quae corporis aut, tempora enim quidem nulla ab! Laborum voluptatum non, vel labore officia veniam fuga eveniet ipsa unde dolores.
+        Maxime consectetur earum odio quod recusandae, magni dolorum ad nemo esse, amet ipsa asperiores, id animi eligendi perferendis delectus voluptatum veniam rem soluta. Tenetur, illo nisi ea eum perferendis vel.</p>
+        <h5 className="center">OUR STORY</h5>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minus perspiciatis optio, ex debitis maiores odit similique accusamus pariatur voluptates ab eveniet ut laudantium repudiandae dolorum voluptas modi nemo voluptate aspernatur?</p>
+    </div>
+);
+    
+export {AboutPage};
