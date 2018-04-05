bb612a5f708376c21ed1f7deaaf41d738da517d8
JelenaMarjanovic <jelena.marjanovic.13@gmail.com>
2018-04-05 11:42:41 +0200

    import materialize

--- ReactBlog_extra/react-bit-blog/src/index.js
+++ ReactBlog_extra/react-bit-blog/src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter } from 'react-router-dom';
 
+import "materialize-css/dist/css/materialize.min.css";
 import './index.css';
+
 import { App } from './app/App';
 import registerServiceWorker from './registerServiceWorker';
 
@@ -10,5 +12,6 @@ ReactDOM.render(
     <HashRouter>
         <App />
     </HashRouter>
-    , document.getElementById('root'));
+    , document.getElementById('root')
+);
 registerServiceWorker();
